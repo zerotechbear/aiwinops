8d20f88d84100c1728b30c5009f3aa6009b5bfc7
Matsu <matuyou0301@gmail.com>
2021-06-02 18:04:30 +0800

    Add Project UI

--- src/App.js
+++ src/App.js
@@ -2,17 +2,21 @@ import './App.css';
 
 import { Route, Switch } from 'react-router-dom';
 
-import LoginForm from './components/UI/Login/LoginForm';
-import ResetForm from './components/UI/Login/ResetForm';
+// import LoginForm from './components/UI/Login/LoginForm';
+// import ResetForm from './components/UI/Login/ResetForm';
+import NavHeader from './components/ProjectPanel/HeaderUI/NavHeader';
 
 function App() {
   return (
     <Switch>
-      <Route path='/' exact>
+      {/* <Route path='/' exact>
         <LoginForm />
       </Route>
       <Route path='/reset'>
         <ResetForm />
+      </Route> */}
+      <Route path='/'>
+        <NavHeader />
       </Route>
     </Switch>
   );
--- src/components/ProjectPanel/HeaderUI/NavHeader.js
+++ src/components/ProjectPanel/HeaderUI/NavHeader.js
@@ -1,23 +1,69 @@
 import classes from '../../../styles/ProjectPanel/HeaderUI/NavHeader.module.css';
 
-import { Link, useHistory } from 'react-router-dom';
+// import { useState } from 'react';
+import { NavLink } from 'react-router-dom';
+
+import { Input, Avatar } from 'antd';
+import { UserOutlined } from '@ant-design/icons';
+
+const navLinks = [
+  {
+    title: 'Home',
+    path: '/home',
+  },
+  {
+    title: 'About',
+    path: '/about',
+  },
+  {
+    title: 'Tutorials',
+    path: '/tutorials',
+  },
+  {
+    title: 'Help',
+    path: '/help',
+  },
+];
 
 const NavHeader = (props) => {
-  const history = useHistory();
+  // const [menuActive, setMenuActive] = useState(false);
+  const { Search } = Input;
 
+  const searchProjectHandler = (value) => {
+    console.log(value);
+  };
   return (
-    <header>
-      <div className={classes.logo}>AIWin</div>
-      <ul>
-        <Link to='/home'>Home</Link>
-        <Link to='/about'>About</Link>
-        <Link to='/Tutorials'>Tutorials</Link>
-        <Link to='/help'>Help</Link>
-        
-      </ul>
-      
+    <header className={classes.header}>
+      <span className={classes.logo}>
+        <NavLink to='/home'>AIWin</NavLink>
+      </span>
+
+      <nav className={classes.nav}>
+        <div className={classes.menu}>
+          <ul>
+            <li>
+              <Search
+                placeholder='Search Project'
+                onSearch={searchProjectHandler}
+              />
+            </li>
+            {navLinks.map((link, index) => (
+              <li key={index}>
+                <NavLink
+                  to={link.path}
+                  activeClassName={classes.active}>
+                  {link.title}
+                </NavLink>
+              </li>
+            ))}
+            <span className={classes.avatar}>
+              <Avatar size={80} icon={<UserOutlined />} />
+            </span>
+          </ul>
+        </div>
+      </nav>
     </header>
-  )
+  );
 };
 
 export default NavHeader;
